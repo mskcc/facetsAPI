27c15bf2f1114362e0db976486956a2afe68e6a6
Adam Price <pricea2@terra.cbio.private>
2022-11-09 12:47:17 -0500

    Cleanup API file.

--- facetsAPI/facetsAPI.py
+++ facetsAPI/facetsAPI.py
@@ -2242,20 +2242,3 @@ class FacetsSample:
 
         
 
-if __name__ == '__main__':
-    clinical_sample_file  = "/work/ccs/shared/resources/impact/cbio_mutations/adam_cron/bsub_run/data_clinical_sample.oncokb.txt"
-    facets_dir            = "/work/ccs/shared/resources/impact/facets/all/"
-
-    #Initialize FacetsMeta. This will build all relevant metadata we need going forward.
-    prepared_metadata = FacetsMeta(clinical_sample_file, facets_dir, True, "purity", "all_meta.dat")
-
-    hypo_dataset = FacetsDataset()
-    filter_on_qc_fail       = True     # If this is True, facets_qc failed samples will be excluded from analysis.
-    purity_filter_thresh    = 0.2      # This is the level of purity at which we disqualify samples from consideration.
-    hypo_dataset.setPurityFilter(purity_filter_thresh)
-    hypo_dataset.setFacetsQCFilter(filter_on_qc_fail)
-    hypo_dataset.buildFacetsDataset(prepared_metadata)
-
-    #Write output.
-    hypo_dataset.writeAlterationData("all_hypoploidy.txt")
-
